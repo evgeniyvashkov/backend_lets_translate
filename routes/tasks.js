9386be056663c737351859e16393eb407daf0088
zhekaprb <zheka_cool@mail.ru>
2019-05-08 01:10:23 +0300

    add post new task

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,4 +5,20 @@ const tasksList = require('../mocks/tasks.json')
 
 router.get('/', (req, res) => res.json(tasksList));
 
+router.post('/', (req, res) => {
+    const { taskId, title, text, dueDate, status } = req.body;
+
+    const newComment = {
+        taskId,
+        title,
+        text,
+        dueDate,
+        status
+    };
+
+    tasksList.push(newComment);
+
+    res.sendStatus(200)
+})
+
 module.exports = router;
