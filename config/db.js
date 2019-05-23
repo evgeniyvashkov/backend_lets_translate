6701ab7b4412e5b7a8567f6921a0e05dd3c5860b
vashkovey <EVashkov@qulix.com>
2019-05-23 16:41:02 +0300

    change connection to DB

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
 const Sequelize = require("sequelize");
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env
 
-const db = new Sequelize(
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  {
-    host: DB_HOST,
-    dialect: "postgres"
-  }
-);
+const DB_NAME = 'lets_translate_db';
+const DB_USER = 'postgres';
+const DB_PASSWORD = '123456';
+const DB_HOST = 'localhost';
+
+//connection to DB
+const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`);
 
 module.exports = {
   db,
